a38f58983c180383c7d82c41e2a33bb10d1ce8c6
Ishwinder-singh-dhingra <55272469+Ishwinder-singh-dhingra@users.noreply.github.com>
2025-05-30 14:45:19 +0530

    Update streamlit_regression.py

--- streamlit_regression.py
+++ streamlit_regression.py
@@ -28,7 +28,7 @@ gender = st.selectbox('Gender',label_encoder_gender.classes_)
 age = st.slider('Age',18,92)
 balance = st.number_input('Balance')
 credit_score= st.number_input('Credit Score')
-estimated_salary = st.number_input('Estimated Salary')
+#estimated_salary = st.number_input('Estimated Salary')
 tenure = st.slider('Tenure',0,10)
 num_of_products = st.slider('Number of Products',1,4)
 has_cr_card= st.selectbox('Has Credit Card',[0,1])
@@ -46,7 +46,7 @@ input_data = pd.DataFrame(
         'NumOfProducts': [num_of_products],
         'HasCrCard': [has_cr_card],
         'IsActiveMember': [is_active_member],
-        'EstimatedSalary': [estimated_salary]
+        #'EstimatedSalary': [estimated_salary]
 
     }
 )
